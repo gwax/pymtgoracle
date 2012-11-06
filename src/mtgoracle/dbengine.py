a48023405729505d61655b26be08e278c6f8e40e
gwax <waksman@gwax.com>
2012-11-06 18:31:42 -0500

    better handling of dbPath

--- src/mtgoracle/dbengine.py
+++ src/mtgoracle/dbengine.py
@@ -1,5 +1,10 @@
 from sqlalchemy import create_engine
+from os.path import expanduser, join, split
 
 
-dbPath = 'mtgoracle.sqlite'
-engine = create_engine('sqlite:///%s' % dbPath, echo=True)
+dbPath = '~/mtgoracle.sqlite'
+dbPath = expanduser(join(*split(dbPath)))
+
+
+def get_engine(path=dbPath, echo=False):
+    return create_engine('sqlite:///%s' % path, echo=echo)
