d5b45add50292da1562486bd8a1e1137a2a8f24f
gwax <waksman@gwax.com>
2012-11-09 17:58:28 -0500

    added all stat columns for Card

--- src/mtgoracle/model/card.py
+++ src/mtgoracle/model/card.py
@@ -1,10 +1,17 @@
-from sqlalchemy import Column, Unicode
+from sqlalchemy import Column, Unicode, PickleType, Integer
 from mtgoracle.model import DeclarativeBase
 
 
 class Card(DeclarativeBase):
     __tablename__ = 'mtgoracle_cards'
     name = Column(Unicode(255), primary_key=True, unique=True, nullable=False)
+    cmc = Column(Integer)
+    cost = Column(Unicode(31))
+    types = Column(PickleType)
+    subtypes = Column(PickleType)
+    rules = Column(PickleType)
+    power = Column(Unicode(5))
+    toughness = Column(Unicode(5))
 
     # {Special methods
     def __repr__(self):
