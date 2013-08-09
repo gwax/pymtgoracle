cc2cfd9b870e2ddb3cf4367965e792c846544fe0
George Waksman <gwaksman@pangenx.com>
2013-08-09 17:14:55 -0400

    Added file information and rdfs:label annotations

--- src/ScrapeAndSerialize.py
+++ src/ScrapeAndSerialize.py
@@ -5,10 +5,10 @@ Created on Aug 9, 2013
 '''
 
 from mtgoracle.rdf.prefix import RDF, ORACLE, MTGSET, nsdict, MAGICCARDSINFO, \
-    XSD, MTGBLOCK, OWL
+    XSD, MTGBLOCK, OWL, RDFS
 from mtgoracle.rdf.store import PersistentStore
 from mtgoracle.scraper import scrape_setdicts
-from rdflib import Literal, URIRef
+from rdflib import Literal, URIRef, Namespace
 from datetime import datetime
 import os.path
 from mtgoracle.rdf import quote_name
@@ -30,21 +30,27 @@ def main():
         setdicts = scrape_setdicts()
         for setdict in setdicts:
             blockuri = MTGBLOCK[quote_name(setdict['block'])]
+            blocklit = Literal(setdict['block'], datatype=XSD.string)
             if (blockuri, RDF.type, ORACLE.Block) not in blockgraph:
                 blockgraph.add((blockuri, RDF.type, ORACLE.Block))
-                blockgraph.add((blockuri, ORACLE.name, Literal(setdict['block'], datatype=XSD.string)))
+                blockgraph.add((blockuri, RDFS.label, blocklit))
+                blockgraph.add((blockuri, ORACLE.name, blocklit))
             seturi = MTGSET[quote_name(setdict['name'])]
+            setlit = Literal(setdict['name'], datatype=XSD.string)
             setgraph.add((seturi, RDF.type, ORACLE.Set))
-            setgraph.add((seturi, ORACLE.name, Literal(setdict['name'], datatype=XSD.string)))
+            setgraph.add((seturi, RDFS.label, setlit))
+            setgraph.add((seturi, ORACLE.name, setlit))
             setgraph.add((seturi, MAGICCARDSINFO.enlink, URIRef(setdict['link'])))
             setgraph.add((seturi, ORACLE.block, blockuri))
         print 'Adding scraping info'
         scrapestr = 'scraping-' + datetime.now().strftime('%Y%m%d')
-        scrapinguri = ORACLE[scrapestr]
-        graph.add((scrapinguri, RDF.type, OWL.Ontology))
-        graph.add((scrapinguri, OWL.imports, ORACLE['']))
+        scrapeuri = ORACLE[scrapestr]
+        graph.bind('', scrapeuri + '#')
+        graph.add((scrapeuri, RDF.type, OWL.Ontology))
+        graph.add((scrapeuri, OWL.imports, ORACLE['']))
+        graph.add((scrapeuri, OWL.versionInfo, Literal('Created with pymtgoracle', datatype=XSD.string)))
         print 'Serializing output'
-        outfile = os.path.join(OUTPATH, scrapestr+'.ttl')
+        outfile = os.path.join(OUTPATH, scrapestr + '.ttl')
         graph.serialize(destination=outfile, format='turtle')
         print 'Done'
 
